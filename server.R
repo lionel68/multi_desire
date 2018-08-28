2e4cca85d9ba3b2424dbaba60628da63e8ba73fc
lhertzog <7189850+lionel68@users.noreply.github.com>
2018-08-28 11:37:13 +0200

    update app with dropping functions capacities

--- server.R
+++ server.R
@@ -7,8 +7,8 @@
 library(shiny)
 library(plyr)
 library(ggplot2)
-library(ggtern)
-library(viridis)
+#library(ggtern)
+#library(viridis)
 
 
 #setwd("Documents/PostDoc_Ghent/Synthesis_stuff/Shiny/multi_desire/")
@@ -34,6 +34,7 @@ dd_mat <- function(ypred,direction,importance,probs=c(0.25,0.5,0.75),type){
   if(type=="div"){
     out <- cbind(out,X_div)
     out$divF <- factor(out$specrich,labels=c(1,2,3))
+    out$fragmF <- factor(out$fragm,labels=c("High","Medium","Low"))
   }
   if(type=="comp"){
     out$speccomb <- rep(comp,each=3)
@@ -66,9 +67,9 @@ shinyServer(function(input, output) {
   #define the importance scores based on the choice of the user (default, all equal or manual)
   importance <- reactive({
     if(input$type == "default"){
-      tmp <- c(1,1,1,1,4,3,7,3.66666666666667,5,3.5,2,3,
-        7.33333333333333,6,6,6,6,4,6.33333333333333,
-        6.66666666666667,9.5,9.5,9.33333333333333,7)
+      tmp <- c(7.5,6.5,6.5,6.5,6,6,9.5,7,4.66666666666667,6.5,6.75,
+               9.33333333333333,9.33333333333333,4,7.25,4.5,2.33333333333333,
+               3,3,4,4,0.666666666666667,0.666666666666667,0.666666666666667)
     }
     
   
@@ -76,10 +77,22 @@ shinyServer(function(input, output) {
     tmp <- rep(1,24)
   }
   if(input$type == "manual"){
-    tmp <- c(input$Egg_bact,input$Egg_IgY,input$Egg_vol,input$Fle_IgY,input$Breed_succ,input$Bird_smi,input$Herbivory,
-      input$Predation,input$Decomp,input$Arth_div,input$Fit_spider,input$Spider_diet,input$C_stock,input$BS,
-      input$pH,input$CN_ratio,input$Phosph,input$Veg_div,input$GLI,input$LAI,input$P_germ,input$Seed_biom,
-      input$Biomass,input$cover)
+    tmp <- c(input$C_stock,input$BS,input$pH,input$CN,input$P,input$Decomp,input$Biomass,input$Cover,
+             input$Veg_div,input$GLI,input$LAI,input$P_germ,input$Seed_biom,input$Arth_div,input$Herbivory,
+             input$Predation,input$Fit_spider,input$Spider_diet,input$Bird_smi,input$Breed_succ,input$Bird_div,input$Egg_vol,
+             input$Egg_bact,input$Egg_IgY)
+  }
+  if(input$type == "drop"){
+    tmp <- c(7.5,6.5,6.5,6.5,6,6,9.5,7,4.66666666666667,6.5,6.75,
+             9.33333333333333,9.33333333333333,4,7.25,4.5,2.33333333333333,
+             3,3,4,4,0.666666666666667,0.666666666666667,0.666666666666667)
+      
+    tmp[!c(input$C_stock2,input$BS2,input$pH2,input$CN2,input$P2,input$Decomp2,input$Biomass2,input$Cover2,
+           input$Veg_div2,input$GLI2,input$LAI2,input$P_germ2,input$Seed_biom2,input$Arth_div2,input$Herbivory2,
+           input$Predation2,input$Fit_spider2,input$Spider_diet2,input$Bird_smi2,input$Breed_succ2,input$Bird_div2,
+           input$Egg_vol2,input$Egg_bact2,input$Egg_IgY2)] <- 0
+    
+   
   }
     return(tmp)
   })
@@ -91,26 +104,27 @@ shinyServer(function(input, output) {
   d_comp <- reactive({dd_mat(pred_comp,dirr,importance(),type="comp")})
   d_kir <- reactive({dd_mat(pred_kir,dirr,importance(),type="kir")})
   #the plots
-  gg_div <- reactive({ggplot(d_div(),aes(x=fragm,y=Med,group=divF)) +
-    geom_ribbon(aes(ymin=LCI,ymax=UCI,fill=divF),alpha=0.2) +
-    geom_path(aes(color=divF)) +
-    labs(fill="Species richness",color="Species richness",x="Fragmentation intensity (from high to low)",y="Desirability score")})
+  gg_div <- reactive({ggplot(d_div(),aes(x=specrich,y=Med,group=fragmF)) +
+    geom_ribbon(aes(ymin=LCI,ymax=UCI,fill=fragmF),alpha=0.2) +
+    geom_path(aes(color=fragmF)) +
+    scale_x_continuous(breaks = c(-0.96,0.45,1.88),labels = 1:3) +
+    labs(fill="Fragmentation intensity",color="Fragmentation intensity",x="Tree species richness",y="Desirability score")})
   
   
-  gg_kir <- reactive({ggtern(d_kir(),aes(fsyl,qrob,qrub))+
-    theme_bw()+
-    geom_tri_tern(bins=4,fun=mean,aes(value=Med,fill=..stat..)) +
-    stat_tri_tern(bins=4,fun=mean,geom="text",aes(value=Med,
-                                                  label=sprintf("%.2f",..stat..)),
-                  color="lightgrey",centroid=TRUE) +
-    scale_fill_viridis(option="C") +
-    theme(legend.position = "none")})
-  
-  gg_comp <- reactive({ggplot(d_comp(),aes(x=fragm,y=Med,group=speccomb)) +
-    geom_ribbon(aes(ymin=LCI,ymax=UCI,fill=speccomb),alpha=0.1) +
-    geom_path(aes(color=speccomb),size=2) +
-    labs(fill="Species\ncomposition",color="Species\ncomposition",x="Fragmentation intensity (from high to low)",y="Desirability score")})
-  
+  # gg_kir <- reactive({ggtern(d_kir(),aes(fsyl,qrob,qrub))+
+  #   theme_bw()+
+  #   geom_tri_tern(bins=4,fun=mean,aes(value=Med,fill=..stat..)) +
+  #   stat_tri_tern(bins=4,fun=mean,geom="text",aes(value=Med,
+  #                                                 label=sprintf("%.2f",..stat..)),
+  #                 color="lightgrey",centroid=TRUE) +
+  #   scale_fill_viridis(option="C") +
+  #   theme(legend.position = "none")})
+  # 
+  # gg_comp <- reactive({ggplot(d_comp(),aes(x=fragm,y=Med,group=speccomb)) +
+  #   geom_ribbon(aes(ymin=LCI,ymax=UCI,fill=speccomb),alpha=0.1) +
+  #   geom_path(aes(color=speccomb),size=2) +
+  #   labs(fill="Species\ncomposition",color="Species\ncomposition",x="Fragmentation intensity (from high to low)",y="Desirability score")})
+  # 
   output$div_f_plot <- renderPlot({
     
     gg_div()
@@ -118,26 +132,28 @@ shinyServer(function(input, output) {
   })
   
   
-  output$kir_f_plot <- renderPlot({
+  #output$kir_f_plot <- renderPlot({
     
-    gg_kir()
+  #  gg_kir()
     
-  })
+  #})
   
   
-  output$comp_f_plot <- renderPlot({
+  #output$comp_f_plot <- renderPlot({
   
-    gg_comp()
+  #  gg_comp()
     
-  })
+  #})
   
   output$dlPlot <- downloadHandler(
     filename = function() paste0("desire_div_",Sys.Date(),".png"),
     content = function(file) {
-      tmp <- grid.arrange(gg_div(),gg_kir(),gg_comp(),ncol=3,bottom=round(importance(),2))
-      ggplot2::ggsave(file,plot=tmp,width=35,height=10,units="cm")
+      tmp <- grid.arrange(gg_div(),ncol=1,bottom=paste("Weights :",paste(round(importance()[1:12],2),collapse = " "),"\n",paste(round(importance()[13:24],2),collapse = " ")))
+      ggplot2::ggsave(file,plot=tmp,width=15,height=10,units="cm")
     },
     contentType = "image/png"
   )
+  
+  output$debug <- renderText(importance())
   
 })
